54c60f18bd6c089f18c2e7ea38b74c0d538eb43f
orkur <orkur123@gmail.com>
2024-01-29 02:09:25 +0100

    non working code, coding in more simple steps

--- src/Main.hs
+++ src/Main.hs
@@ -17,24 +17,34 @@
 
 module Main where
 
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Logger (LoggingT, runStdoutLoggingT)
-import Data.Aeson (KeyValue ((.=)), Value (String), object)
+import Control.Monad.Trans.Class (lift)
+import Crypto.BCrypt (HashingPolicy (HashingPolicy), hashPassword, hashPasswordUsingPolicy, slowerBcryptHashingPolicy)
+import qualified Crypto.BCrypt as BCrypt
+import Data.Aeson (KeyValue ((.=)), Value (Bool, String), object)
+import Data.ByteString.Char8 (pack)
 import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Database.Persist (PersistQueryRead (selectFirst))
 import Database.Persist as Persist hiding (get)
 import Database.Persist.Sql (delete)
-import Database.Persist.Sqlite hiding (get)
+import Database.Persist.Sqlite hiding (get, insert)
 import Database.Persist.TH
-import Network.HTTP.Types.Status (status400)
+import Network.HTTP.Types.Status
 import Web.Spock as Web
+import Web.Spock.Action
 import Web.Spock.Config (PoolOrConn (PCPool), defaultSpockCfg)
 
 share
   [mkPersist sqlSettings, mkMigrate "migrateAll"]
   [persistLowerCase|
-Person json -- The json keyword will make Persistent generate sensible ToJSON and FromJSON instances for us.
-  name Text
-  age Int
-  deriving Show
+User json
+    name Text
+    email Text
+    password Text
+    isAdmin Bool
+    deriving Show
 |]
 
 type Api = SpockM SqlBackend () () ()
@@ -50,25 +60,44 @@ main = do
 
 app :: Api
 app = do
-  get "people" $ do
-    allPeople <- runSQL $ selectList [] [Asc PersonId]
-    json allPeople
-  post "people" $ do
-    maybePerson <- jsonBody' :: ApiAction (Maybe Person)
-    case maybePerson of
-      Nothing -> do
-        setStatus status400
-        text "Bad request!"
-      Just thePerson -> do
-        newId <- runSQL $ insert thePerson
-        json $ object ["result" .= String "success", "id" .= newId]
-  Web.delete "people" $ do
-    maybePersonId <- param "id" :: ApiAction (Maybe PersonId)
-    case maybePersonId of
-      Nothing -> errorJson 1 "Person ID not provided"
-      Just personId -> do
-        runSQL $ Database.Persist.Sql.delete personId
-        json $ object ["result" .= String "success"]
+  post "register" $ do
+    newUser <- jsonBody' :: ApiAction (Maybe User)
+    case newUser of
+      Nothing ->
+        setStatus status400 >> text "Non valid user"
+      Just user -> do
+        result <- registerUser user
+        case result of
+          Left err -> setStatus status400 >> text err
+          Right _ -> setStatus status200 >> text "registered"
+
+registerUser :: User -> ApiAction (Either Text ())
+registerUser user =
+  if True
+    then return $ Left "Error!"
+    else return $ Right ()
+
+-- registerUser :: User -> SqlPersistT (LoggingT IO) (Either Text ())
+-- registerUser user = do
+--   existsUser <- runSQL $ selectFirst [UserName ==. userName user] []
+--   case existsUser of
+--     Just _ -> return $ Left "User exists!"
+--     Nothing -> do
+--       existsMail <- runSQL $ selectFirst [UserEmail ==. userEmail user] []
+--       case existsMail of
+--         Just _ -> return $ Left "Mail exists!"
+--         Nothing -> do
+--           hashedPassword <- hashPassword' (userPassword user)
+--           case hashedPassword of
+--             Nothing -> lift $ return $ Left "there's problem with password, please use latin characters!"
+--             Just existsPassword -> do
+--               runSQL $ insert (User (userName user) (userEmail user) existsPassword False)
+--               return $ Right ()
+
+hashPassword' :: Text -> SqlPersistT (LoggingT IO) (Maybe Text)
+hashPassword' password = do
+  hashedPassword <- liftIO $ hashPasswordUsingPolicy slowerBcryptHashingPolicy (encodeUtf8 password)
+  return $ decodeUtf8 <$> hashedPassword
 
 runSQL ::
   (HasSpock m, SpockConn m ~ SqlBackend) =>
